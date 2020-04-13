7562163117e4b9ebd207409e2f20a7925d8671a0
Ricardo <59628818+Rpinto02@users.noreply.github.com>
2020-04-13 15:24:04 -0300

    Add files via upload
    Script for tables and entities creation with mysql.

---
+++ CreateTables.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE NextDataEngineering;
+
+CREATE TABLE NextDataEngineering.Situacao(
+situacao_id    int         NOT NULL,
+descricao      varchar(50) NOT NULL,
+_created       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+_updated       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (situacao_id)
+);
+
+CREATE TABLE NextDataEngineering.Pessoa(
+pessoa_id     int         NOT NULL AUTO_INCREMENT,
+Pessoa        varchar(50) NOT NULL,
+situacao_id   int         NOT NULL,
+_created      DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+_updated      DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (pessoa_id)
+);
+
+
+CREATE TABLE NextDataEngineering.HistoricoSituacao(
+Pessoa         varchar(50) NOT NULL,
+DATA           datetime    NOT NULL,
+situacao_id    int         NOT NULL,
+_created       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+_updated       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE NextDataEngineering.RegistroLocalizacao (
+Pessoa    varchar(50)   NOT NULL,
+DATA      datetime      NULL,
+lat       double(7, 5)  NULL,
+lng       double(7, 5)  NULL,
+situacao  int           NOT NULL,
+_created  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+_updated  DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE NextDataEngineering.LimitesDeslocamento(
+Pessoa    varchar(50)    NOT NULL,
+lat1      double(7, 5)   NULL,
+lng1      double(7, 5)   NULL,
+raio      double(7, 5)   NULL
+);
+
