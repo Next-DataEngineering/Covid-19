52d91baff048d4364cd4adebbad05a45de2228bf
Ricardo <59628818+Rpinto02@users.noreply.github.com>
2020-04-13 15:37:46 -0300

    Add files via upload
    Created indexes for previous tables

---
+++ CreateIndexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_pessoa ON NextDataEngineering.RegistroLocalizacao (Pessoa);
+
+CREATE INDEX idx_pessoa ON NextDataEngineering.HistoricoSituacao (Pessoa);
+
+CREATE INDEX idx_pessoa ON NextDataEngineering.LimitesDeslocamento (Pessoa);
