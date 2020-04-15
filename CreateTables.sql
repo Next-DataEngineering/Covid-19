ffd946a6460d0a266ef577feaa81aef60d21bead
Ricardo <59628818+Rpinto02@users.noreply.github.com>
2020-04-15 13:48:37 -0300

    Add files via upload
    Changed the primary keys to a primary keys with a single value.

--- CreateTables.sql
+++ CreateTables.sql
@@ -9,31 +9,29 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE NextDataEngineering.Pessoa(
-id     int         NOT NULL AUTO_INCREMENT,
+id            int         NOT NULL AUTO_INCREMENT,
 Pessoa        varchar(50) NOT NULL,
 situacao_id   int         NOT NULL,
 _created      DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 _updated      DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-PRIMARY KEY (id, Pessoa),
+PRIMARY KEY (id),
 FOREIGN KEY (situacao_id) REFERENCES Situacao(id) ON UPDATE CASCADE
 );
 
 
 CREATE TABLE NextDataEngineering.HistoricoSituacao(
 pessoa_id      int         NOT null,
-Pessoa         varchar(50) NOT NULL,
 DATA           datetime    NOT NULL,
 situacao_id    int         NOT NULL,
 _created       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 _updated       DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (situacao_id) REFERENCES Situacao(id)             ON UPDATE CASCADE,
-FOREIGN KEY (pessoa_id, Pessoa) REFERENCES Pessoa(id, Pessoa) ON UPDATE cascade
+FOREIGN KEY (pessoa_id)   REFERENCES Pessoa(id)               ON UPDATE cascade
 );
 
 
 CREATE TABLE NextDataEngineering.RegistroLocalizacao (
 pessoa_id      int           NOT null,
-Pessoa         varchar(50)   NOT NULL,
 DATA           datetime      NULL,
 lat            double(7, 5)  NULL,
 lng            double(7, 5)  NULL,
@@ -41,18 +39,14 @@ situacao_id    int           NOT NULL,
 _created       DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
 _updated       DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 FOREIGN KEY (situacao_id) REFERENCES Situacao(id)             ON UPDATE CASCADE,
-FOREIGN KEY (pessoa_id, Pessoa) REFERENCES Pessoa(id, Pessoa) ON UPDATE cascade
+FOREIGN KEY (pessoa_id)   REFERENCES Pessoa(id)               ON UPDATE cascade
 );
 
 CREATE TABLE NextDataEngineering.LimitesDeslocamento(
 pessoa_id      int            NOT null,
-Pessoa         varchar(50)    NOT NULL,
 lat1           double(7, 5)   NULL,
 lng1           double(7, 5)   NULL,
 raio           double(7, 5)   NULL,
-FOREIGN KEY (pessoa_id, Pessoa) REFERENCES Pessoa(id, Pessoa) ON UPDATE cascade
+FOREIGN KEY (pessoa_id) REFERENCES Pessoa(id) ON UPDATE cascade
 );
-
-
-
 
