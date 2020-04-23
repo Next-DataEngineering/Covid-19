d695c289683253fa58124580de9bb77b6b71f277
Pedro <pedro.h.g.alexandre@outlook.com>
2020-04-23 20:33:01 -0300

    Querys de Localização e Comparação

--- QueryComparar.sql
+++ QueryComparar.sql
@@ -1,15 +1,12 @@
-SELECT NextDataEngineering.RegistroLocalizacao.pessoa_id,
-NextDataEngineering.RegistroLocalizacao.lat,
-NextDataEngineering.RegistroLocalizacao.long,
-NextDataEngineering.RegistroLocalizacao._updated INTO LocalizacaoBackUp
-FROM NextDataEngineering.RegistroLocalizacao
+SELECT pessoa_id, lat, long, _updated INTO LocalizacaoBackUp
+FROM RegistroLocalizacao
 
 SELECT *
-	FROM NextDataEngineering.RegistroLocalizacao, LocalizacaoBackUp
+	FROM RegistroLocalizacao, LocalizacaoBackUp
 	GROUP BY LocalizacaoBackUp.pessoa_id
-	WHERE MINUTE(NextDataEngineering.RegistroLocalizacao._updated)-MINUTE(LocalizacaoBackUp._update) <=30 AND 
-		NextDataEngineering.RegistroLocalizacao.lat-LocalizacaoBackUp.lat <X AND
-		NextDataEngineering.RegistroLocalizacao.long-LocalizacaoBackUp.long <Y AND NOT
-		NextDataEngineering.RegistroLocalizacao.pessoa_id=LocalizacaoBackUp.pessoa_id
+	WHERE MINUTE(RegistroLocalizacao._updated)-MINUTE(LocalizacaoBackUp._update) <=30 AND 
+		RegistroLocalizacao.lat-LocalizacaoBackUp.lat <X AND
+		RegistroLocalizacao.long-LocalizacaoBackUp.long <Y AND NOT
+		RegistroLocalizacao.pessoa_id=LocalizacaoBackUp.pessoa_id
         ;
 ;
